27e7990cc450ee5fe6f8f43d6aa1fa5c9aa64e1a
shmoon-hub <shmoongh01@gmail.com>
2023-12-11 21:13:31 +0900

    expo 음악모듈 설치및  start.js 사운드 기능 추가 -현석우

--- MovieMakeU-app/Start.js
+++ MovieMakeU-app/Start.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import * as Font from 'expo-font';
+import { Audio } from 'expo-av';
 
-export default function Start() {
-  const navigation = useNavigation();
-  const [fontLoaded, setFontLoaded] = useState(false);
+export default function Start({ navigation }) {
+  const [sound, setSound] = useState();
 
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        'Amarillo': require('./assets/fonts/Amarillo.ttf'), // 폰트 파일의 경로를 정확하게 지정하세요.
-      });
-      setFontLoaded(true);
-    }
-    loadFonts();
-  }, []);
-
-  if (!fontLoaded) {
-    return <View style={styles.container}><Text>Loading...</Text></View>; // 폰트 로딩 중 화면
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/sound/click-sound.mp3') // 여기를 변경했습니다
+    );
+    setSound(sound);
+  
+    await sound.playAsync();
   }
+  
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync(); 
+        }
+      : undefined;
+  }, [sound]);
 
   return (
     <ImageBackground 
-      source={{ uri: 'https://media.istockphoto.com/id/1478374885/ko/%EC%82%AC%EC%A7%84/%EC%98%81%ED%99%94%EA%B4%80%EC%97%90%EC%84%9C-%EC%98%81%ED%99%94%EB%A5%BC-%EB%B3%B4%EB%8A%94-%EC%A6%90%EA%B1%B0%EC%9A%B4-%EA%B0%80%EC%A1%B1.jpg?s=2048x2048&w=is&k=20&c=2nozAq0obiM6RsQcvmTnTURmfGmIzgPsudOQVAl0mY8=' }}
+    source={{ uri: 'https://media.istockphoto.com/id/1478374885/ko/%EC%82%AC%EC%A7%84/%EC%98%81%ED%99%94%EA%B4%80%EC%97%90%EC%84%9C-%EC%98%81%ED%99%94%EB%A5%BC-%EB%B3%B4%EB%8A%94-%EC%A6%90%EA%B1%B0%EC%9A%B4-%EA%B0%80%EC%A1%B1.jpg?s=2048x2048&w=is&k=20&c=2nozAq0obiM6RsQcvmTnTURmfGmIzgPsudOQVAl0mY8=' }} // 이미지 URL
       style={styles.background}
     >
       <View style={styles.container}>
@@ -31,10 +33,22 @@ export default function Start() {
           <Text style={styles.logoText}>MovieMakeU</Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              playSound();
+              navigation.navigate('Login');
+            }}
+          >
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignUp')}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              playSound();
+              navigation.navigate('SignUp');
+            }}
+          >
             <Text style={styles.buttonText}>Sign Up</Text>
           </TouchableOpacity>
         </View>
@@ -42,7 +56,6 @@ export default function Start() {
     </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
   background: {
     flex: 1,
@@ -61,8 +74,7 @@ const styles = StyleSheet.create({
   logoText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'white',
-    fontFamily: 'Amarillo', // 커스텀 폰트 적용
+    color: 'white', // Change text color to white
   },
   buttonContainer: {
     width: '80%',
@@ -72,12 +84,12 @@ const styles = StyleSheet.create({
   button: {
     marginBottom: 10,
     paddingVertical: 12,
-    backgroundColor: 'white',
+    backgroundColor: 'white', // Instagram-like button color
     borderRadius: 20,
     alignItems: 'center',
   },
   buttonText: {
     fontSize: 16,
-    color: '#000',
+    color: '#000', // Text color
   },
 });
