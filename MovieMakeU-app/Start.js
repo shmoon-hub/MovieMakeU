12a4c56da2abdeb82ba04a1ad28fb49b77fb1900
shmoon-hub <shmoongh01@gmail.com>
2023-12-12 12:39:48 +0900

    start 메인화면에서 글꼴 적용 추가

--- MovieMakeU-app/Start.js
+++ MovieMakeU-app/Start.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as Font from 'expo-font';
 import { Audio } from 'expo-av';
 
-export default function Start({ navigation }) {
+export default function Start() {
+  const navigation = useNavigation();
+  const [fontLoaded, setFontLoaded] = useState(false);
   const [sound, setSound] = useState();
 
   async function playSound() {
@@ -23,9 +27,23 @@ export default function Start({ navigation }) {
       : undefined;
   }, [sound]);
 
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'Amarillo': require('./assets/fonts/Amarillo.ttf'), // 폰트 파일의 경로를 정확하게 지정하세요.
+      });
+      setFontLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontLoaded) {
+    return <View style={styles.container}><Text>Loading...</Text></View>; // 폰트 로딩 중 화면
+  }
+
   return (
     <ImageBackground 
-    source={{ uri: 'https://media.istockphoto.com/id/1478374885/ko/%EC%82%AC%EC%A7%84/%EC%98%81%ED%99%94%EA%B4%80%EC%97%90%EC%84%9C-%EC%98%81%ED%99%94%EB%A5%BC-%EB%B3%B4%EB%8A%94-%EC%A6%90%EA%B1%B0%EC%9A%B4-%EA%B0%80%EC%A1%B1.jpg?s=2048x2048&w=is&k=20&c=2nozAq0obiM6RsQcvmTnTURmfGmIzgPsudOQVAl0mY8=' }} // 이미지 URL
+      source={{ uri: 'https://media.istockphoto.com/id/1478374885/ko/%EC%82%AC%EC%A7%84/%EC%98%81%ED%99%94%EA%B4%80%EC%97%90%EC%84%9C-%EC%98%81%ED%99%94%EB%A5%BC-%EB%B3%B4%EB%8A%94-%EC%A6%90%EA%B1%B0%EC%9A%B4-%EA%B0%80%EC%A1%B1.jpg?s=2048x2048&w=is&k=20&c=2nozAq0obiM6RsQcvmTnTURmfGmIzgPsudOQVAl0mY8=' }}
       style={styles.background}
     >
       <View style={styles.container}>
@@ -33,22 +51,10 @@ export default function Start({ navigation }) {
           <Text style={styles.logoText}>MovieMakeU</Text>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              playSound();
-              navigation.navigate('Login');
-            }}
-          >
+        <TouchableOpacity style={styles.button} onPress={() => {playSound(); navigation.navigate('Login'); }}>
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              playSound();
-              navigation.navigate('SignUp');
-            }}
-          >
+          <TouchableOpacity style={styles.button} onPress={() => {playSound(); navigation.navigate('SignUp'); }}>
             <Text style={styles.buttonText}>Sign Up</Text>
           </TouchableOpacity>
         </View>
@@ -56,6 +62,7 @@ export default function Start({ navigation }) {
     </ImageBackground>
   );
 }
+
 const styles = StyleSheet.create({
   background: {
     flex: 1,
@@ -74,7 +81,8 @@ const styles = StyleSheet.create({
   logoText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'white', // Change text color to white
+    color: 'white',
+    fontFamily: 'Amarillo', // 커스텀 폰트 적용
   },
   buttonContainer: {
     width: '80%',
@@ -84,12 +92,12 @@ const styles = StyleSheet.create({
   button: {
     marginBottom: 10,
     paddingVertical: 12,
-    backgroundColor: 'white', // Instagram-like button color
+    backgroundColor: 'white',
     borderRadius: 20,
     alignItems: 'center',
   },
   buttonText: {
     fontSize: 16,
-    color: '#000', // Text color
+    color: '#000',
   },
 });
